a7d031c15682687c6e825b18565753204e3aa9d7
Randoraz <97787227+Randoraz@users.noreplay.github.com>
2022-01-23 01:41:42 +0900

    Add intro sentence and more sentence possibilities

--- message_generator.js
+++ message_generator.js
@@ -1,11 +1,12 @@
 //Message Generator
 
-const firstString = ['You should', 'It\'d be good if you could', 'You should not', 'You have to', 'You absolutely can\'t'];
-const secondString = ['trust', 'forget about', 'love', 'ignore', 'be friends with'];
-const thirdString = ['anyone', 'everything', 'your heart', 'your job', 'your head'];
+const introString = ['I think', 'I calculate that', 'I saw in my dreams that', 'A little bird told me that', 'My virtual cards said'];
+const firstString = ['you should', 'it\'d be good if you could', 'you should not', 'you have to', 'you absolutely can\'t'];
+const secondString = ['trust', 'forget about', 'love', 'ignore', 'be friends with', 'work hard for', 'enjoy', 'have fun with'];
+const thirdString = ['anyone', 'everything', 'your heart', 'your job', 'your head', 'your family', 'nature'];
 
-const randomMessage = (randString1, randString2, randString3) => {
-    return `${randString1} ${randString2} ${randString3}!`;
+const randomMessage = (randStringIntro, randString1, randString2, randString3) => {
+    return `${randStringIntro} ${randString1} ${randString2} ${randString3}!`;
 }
 
 const selectRandomString = arr => {
@@ -13,4 +14,4 @@ const selectRandomString = arr => {
     return arr[randNum];
 }
 
-console.log(randomMessage(selectRandomString(firstString), selectRandomString(secondString), selectRandomString(thirdString)));
+console.log(randomMessage(selectRandomString(introString), selectRandomString(firstString), selectRandomString(secondString), selectRandomString(thirdString)));
