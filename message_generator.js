89b4e78c5b75b271d3f383920c668bdf78b74aa9
Randoraz <97787227+Randoraz@users.noreplay.github.com>
2022-01-23 00:57:19 +0900

    Create messages and functions

---
+++ message_generator.js
@@ -0,0 +1,14 @@
+//Message Generator
+
+const firstString = ['You should', 'It\'d be good if you could', 'You should not', 'You have to', 'You absolutely can\'t'];
+const secondString = ['trust', 'forget about', 'love', 'ignore', 'be friends with'];
+const thirdString = ['anyone', 'everything', 'your heart', 'your job', 'your head'];
+
+const randomMessage = (randString1, randString2, randString3) => {
+    return `${randString1} + ${randString2} + ${randString3}!`;
+}
+
+const selectRandomString = arr => {
+    const randNum = Math.floor(Math.random * arr.length);
+    return arr[randNum];
+}
