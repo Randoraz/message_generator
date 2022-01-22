3bb39fc7c7f0376f4ff06812e02e20d7f8fb74f5
Randoraz <97787227+Randoraz@users.noreplay.github.com>
2022-01-23 01:24:02 +0900

    Fix functions and add print message

--- message_generator.js
+++ message_generator.js
@@ -5,10 +5,12 @@ const secondString = ['trust', 'forget about', 'love', 'ignore', 'be friends wit
 const thirdString = ['anyone', 'everything', 'your heart', 'your job', 'your head'];
 
 const randomMessage = (randString1, randString2, randString3) => {
-    return `${randString1} + ${randString2} + ${randString3}!`;
+    return `${randString1} ${randString2} ${randString3}!`;
 }
 
 const selectRandomString = arr => {
-    const randNum = Math.floor(Math.random * arr.length);
+    const randNum = Math.floor(Math.random() * arr.length);
     return arr[randNum];
 }
+
+console.log(randomMessage(selectRandomString(firstString), selectRandomString(secondString), selectRandomString(thirdString)));
